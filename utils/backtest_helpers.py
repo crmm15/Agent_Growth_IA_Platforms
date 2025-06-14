2ac6c16068aa4799ad1ef655fab1416e034d78a4
crmm15 <crmm15@gmail.com>
2025-06-14 00:27:47 -0400

    Update backtest_helpers.py
    Release 1 (v1.0)

--- utils/backtest_helpers.py
+++ utils/backtest_helpers.py
@@ -21,7 +21,7 @@ def robust_trend_filter(df):
         df.loc[df['mavilimw'].notna(), 'Close'] >
         df.loc[df['mavilimw'].notna(), 'mavilimw']
     )
-    # Para primeras señales: si las últimas 3 velas (incluida la actual)
+    # Para primeras señales: si las últimas 3 velas (incluida la actual) 
     # están por arriba de la primera mavilimw válida
     first_valid = df['mavilimw'].first_valid_index()
     if first_valid is not None and first_valid >= 2:
